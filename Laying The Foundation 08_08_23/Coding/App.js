8a89cbe6497685ec6ad09f6c0979024eeb29eb8f
Deepak <deepaknnayak34@gmail.com>
2023-08-22 17:40:24 +0530

    episode 3

--- Laying The Foundation 08_08_23/Coding/App.js
+++ Laying The Foundation 08_08_23/Coding/App.js
@@ -15,5 +15,34 @@ const create_element = React.createElement(
 
 )
 
+
+// How to create an JSX 
+
+const heading = (
+    <>
+        <h1>💙 Namestey React 💛</h1>
+        <ul>
+            <li>Course Name: Namestey React</li>
+            <li>Mentor Name: Akshay Saini</li>
+            <li>Course Fee: 2100</li>
+            <li> <a href="https://namastedev.com/namaste-react/">Website Link</a></li>
+        </ul>
+    </>
+)
+
+
+// React Functional Component 
+
+const HeadingComponent = () =>{
+    return(
+        <>
+            <h1>This is React Functional Component</h1>
+        </>
+    )
+}
+const HeadingComponent2 = () => (<h1>This is React Functional Component</h1>)
+
+
+
 const root = ReactDom.createRoot(document.getElementById('root'))
-root.render(create_element)
+root.render(heading)
