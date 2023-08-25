b0ef7d016da4c89cbed767d91b7221f255822a80
Deepak <deepaknnayak34@gmail.com>
2023-08-26 02:22:06 +0530

    episode 4

--- Talk Is Cheap Show Me Code/Coding/App.js
+++ Talk Is Cheap Show Me Code/Coding/App.js
@@ -6,7 +6,8 @@ const Header = () =>{
     return(
         <div className="header">
             <div className="logo_container">
-                <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=food&sf="/>
+                <img className="logo" src="https://www.theknowhowlib.com/wp-content/uploads/2020/05/Swiggy-2.png"/>
+
             </div>
             <div className="nav_items">
                 <ul>
