bb3b1d08a9926b260da42702f32bd1bd3e397568
Deepak <deepaknnayak34@gmail.com>
2023-09-07 16:45:23 +0530

    episode 4

--- Talk Is Cheap Show Me Code/Coding/App.js
+++ Talk Is Cheap Show Me Code/Coding/App.js
@@ -1,21 +1,91 @@
 import React from 'react'
 import ReactDOM  from 'react-dom/client'
-import Header from './src/Component/Header.js'
-import Body from './src/Component/Body.js'
-import DownloadComponent from './src/Component/DownloadComponent.js'
-import FooterComponent from "./src/Component/FooterComponent.js"
+import ResturantLogo from '../Coding/src/Images/ResturantLogo.jpg'
 
 
 
+/**
+ * Header
+ * - logo
+ * - nav-items
+ * Body
+ * - Search
+ * - Resturant Container
+ * - Resturant Card
+ *      - Image 
+ *      - Name Of The Restaurant
+ *      - rating
+ *      - Star Rating
+ *      - Cuisine 
+ * Footer
+ * - Copyight
+ * - Links
+ * - Address
+ * - Contact Information
+ */
 
-const AppLayout = () => (
-    <>
-        <Header/>
-        <Body/>
-        <DownloadComponent/>
-        <FooterComponent/>
-    </>
-)
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className='logo_container'>
+                <img className='logo' src={ResturantLogo}></img>
+                <h1>Food Villa</h1>
+            </div>
+            <div className='nav_items'>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+
+const ResturantCard = () => {
+    return (
+        <div className="res-card">
+            <ul class="card_list_items">
+                <li>
+                    <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/0984acc0ed7b914206dbbcb90297becc"/>
+                    </li>
+                <li>Meghna Foods</li>
+                <li>Rating</li>
+                <li>Cuisines</li>
+                <li>Place: Tollygunge</li>
+            </ul>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="Body maxWidth">
+            <div className="search_bar">
+                Search
+            </div>
+            <div className="resturant_container">
+                <ResturantCard />
+                <ResturantCard />
+                <ResturantCard />
+                <ResturantCard />
+            </div>
+        </div>
+    )
+}
+
+
+const AppLayout = () => {
+    return (
+        <>
+            <Header />
+            <Body />
+        </>
+    )
+}
 
 
 
