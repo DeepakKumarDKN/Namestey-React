0cd7f391479a9d90ed61d1070a40e3ccf99c83fc
Deepak <deepaknnayak34@gmail.com>
2023-09-14 23:16:40 +0530

    code added

--- Exploring The World/Coding/src/Component/Body.js
+++ Exploring The World/Coding/src/Component/Body.js
@@ -11,11 +11,15 @@ import Shimmer from "../utils/Shimmer";
 const Body = () => {
     
 let [ListofResturants, setListofResturants] = useState([])
+const [filteredResturants, setfilteredResturants] = useState([])
 let [searchValue, setsearchValue] = useState("")
     
     useEffect(()=>{
-        fetchData()
+        fetchData(),
+        filteredData()
     },[])
+
+    
 
     const fetchData = async () => {
         const data =  await fetch(API_LINK);
@@ -28,7 +32,21 @@ let [searchValue, setsearchValue] = useState("")
                 return setListofResturants(checkedData);
             }
         }
+
     }
+
+    const filteredData = async () => {
+        const filterdata = await fetch (API_LINK)
+        const jsondata = await filterdata.json()
+
+        for(let i=0; i<jsondata.data.cards.length; i++){
+            let filteredchecked = jsondata?.data?.cards[i]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+            if(filteredchecked !== undefined){
+                return setfilteredResturants(filteredchecked)
+            }
+        }
+    }
+
 
  
     return ListofResturants.length === 0 ? <Shimmer/> : (
@@ -36,11 +54,13 @@ let [searchValue, setsearchValue] = useState("")
             <div className="button_container">
                 <div className="search_bar">
                     <input type="text" 
-                        placeholder="Search"
+                        placeholder="Search Resturants Name Here"
                         value = {searchValue} 
                         onChange={(e)=>{setsearchValue(e.target.value)}} 
                     />
-                    <button className="submit_button">Submit</button>
+                    <button className="submit_button" onClick={()=> {
+                        setfilteredResturants(ListofResturants.filter((res)=> res.info.name.toLowerCase().includes(searchValue.toLowerCase())))
+                    }}>Search</button>
                 </div>
                 <div>
                     <button className="top_res" onClick={() => {
@@ -52,7 +72,7 @@ let [searchValue, setsearchValue] = useState("")
             </div>
             <div className="resturant_container">
                 {
-                    ListofResturants.map((resturant)=>(
+                    filteredResturants.map((resturant)=>(
                         <ResturantCard key = {resturant?.info?.id}
                         resData = {resturant}/>
                     ))
