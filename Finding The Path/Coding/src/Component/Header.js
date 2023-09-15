760a6b66cc64cf59c8e0f99190754608e0e70588
Deepak <deepaknnayak34@gmail.com>
2023-09-15 23:22:39 +0530

    finding the path

---
+++ Finding The Path/Coding/src/Component/Header.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import ResturantLogo from '../Images/ResturantLogo.jpg'
+
+
+const Header = () => {
+
+    const [buttonName, setbuttonName] = useState('Login')
+    console.log('header rendered')
+    useEffect(()=> {
+        console.log('use effect called')
+    },[])
+
+    return (
+        <div className="header">
+            <div className='logo_container'>
+                <img className='logo' src={ResturantLogo}></img>
+                <h1>Food Villa</h1>
+            </div>
+            <div className='nav_items'>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                    <button className="login_logout" 
+                    onClick={()=> {
+                        buttonName === "Login" ? setbuttonName('Logout') : setbuttonName('Login')
+                    }}>{buttonName}</button>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
