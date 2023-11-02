f5f1e0c76603b73ecadb29c951fc958e3a347811
Deepak <deepaknnayak34@gmail.com>
2023-11-02 10:41:15 +0530

    updated

--- Finding The Path/Coding/src/Component/Contact.js
+++
@@ -1,8 +0,0 @@
-const Contact =()=>{
-    return(
-        <div className="contact_container maxWidth">
-            <h1> This is Contact Page</h1>
-        </div>
-    )
-}
-export default Contact;
